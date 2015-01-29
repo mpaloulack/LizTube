27f300ec9e1f0eef86e3f2968090147b86490868
spywen <spywen@hotmail.fr>
2015-01-29 23:02:24 +0100

    Configuration BE

---
+++ src/main/java/com/liztube/config/AppConfigs.java
@@ -0,0 +1,55 @@
+package com.liztube.config;
+
+import com.liztube.utils.jackson.HibernateAwareObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.List;
+
+/**
+ * Created by laurent on 06/07/2014.
+ */
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.liztube.controller"})
+@Configuration
+@EnableScheduling
+public class AppConfigs extends WebMvcConfigurerAdapter {
+    private static final int CACHE_PERIOD = 31556926;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //registry.addResourceHandler("/css/**").addResourceLocations("/app/css/").setCachePeriod(CACHE_PERIOD);
+        //registry.addResourceHandler("/js/**").addResourceLocations("/app/js/").setCachePeriod(CACHE_PERIOD);
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+    public void configureMessageConverters (List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setObjectMapper(new HibernateAwareObjectMapper());
+        converters.add(mappingJackson2HttpMessageConverter);
+        converters.add(new StringHttpMessageConverter());
+    }
+}
---
+++ src/main/java/com/liztube/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.liztube.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public ModelAndView SingleApplicationPage() {
+        ModelAndView modelAndView = new ModelAndView("index");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writerWithDefaultPrettyPrinter();
+
+        return modelAndView;
+    }
+}
