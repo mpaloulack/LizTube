503560fd5cfde9aee4fa90cb6374f9f464e9a6e3
Laurent <spywen@hotmail.fr>
2015-02-25 23:59:40 +0100

    try to fix unknow file

---
+++ src/main%90java/com/liztube/service/AuthService.java
@@ -0,0 +1,48 @@
+package com.liztube.service;
+
+import com.liztube.business.AuthBusiness;
+import com.liztube.exception.UserNotFoundException;
+import com.liztube.utils.facade.SigninTestExistFacade;
+import com.liztube.utils.facade.UserConnectedProfile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Service which provide all the method concerning the authentication of an user (login, signin, get user profile...)
+ */
+@RestController
+@RequestMapping("/api/auth")
+public class AuthService {
+    @Autowired
+    AuthBusiness authBusiness;
+
+    /**
+     * Get current user profile
+     * @return
+     * @throws UserNotFoundException
+     */
+    @RequestMapping(value = "/currentProfil",method = RequestMethod.GET)
+    public UserConnectedProfile getUserProfil() {
+        return authBusiness.getUserConnectedProfile();
+    }
+
+    /**
+     * Determine if a pseudo is already used
+     * @param signinTestExistFacade
+     * @return
+     */
+    @RequestMapping(value = "/pseudo", method = RequestMethod.POST)
+    public Boolean existPseudo(@RequestBody SigninTestExistFacade signinTestExistFacade){
+        return authBusiness.existPseudo(signinTestExistFacade);
+    }
+
+    /**
+     * Determine if an email is already used
+     * @param signinTestExistFacade
+     * @return
+     */
+    @RequestMapping(value = "/email", method = RequestMethod.POST)
+    public Boolean existEmail(@RequestBody SigninTestExistFacade signinTestExistFacade){
+        return authBusiness.existEmail(signinTestExistFacade);
+    }
+}
