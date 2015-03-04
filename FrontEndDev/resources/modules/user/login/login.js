16bfd1678f619f1e607a206348d710669a91bcce
mpaloulack <mpaloulack@gmail.com>
2015-03-04 12:43:46 +0100

    Ajouts ngMessages et implémentation dans login

--- FrontEndDev/resources/modules/user/login/login.js
+++ FrontEndDev/resources/modules/user/login/login.js
@@ -1,33 +1,46 @@
 /**
  * Created by Youcef on 26/02/2015.
  */
-angular.module("liztube.login",["ngRoute", "liztube.dataService.authService"])
-    .controller("loginCtrl", function($scope, $location, authService, $window) {
+angular.module("liztube.login",[
+    "ngRoute", 
+    "liztube.dataService.authService",
+    'ngMessages'
+]).controller("loginCtrl", function($scope, $location, authService, $window, $interval) {
 
-        $scope.submit= function() {
+    $scope.loaderLogin= false;
+
+    $scope.submit= function() {
+        $scope.loaderLogin= true;
+
+        $interval(function(){
+            console.log($scope.loaderLogin);
             authService.login($scope.login, $scope.password).then(function(){
-                console.log("success");
-                authService.currentUser().then(function(currentUser){
-                    console.log("currentUser success " +currentUser.pseudo);
-                    $window.user = currentUser;
-                },function(){
-                    console.log("currentUser fail");
-                });
-                $location.path('/');
+            console.log("success");
+            authService.currentUser().then(function(currentUser){
+                console.log("currentUser success " +currentUser.pseudo);
+                $window.user = currentUser;
+            },function(){
+                console.log("currentUser fail");
+            });
+            $location.path('/');
             },function(){
                 console.log("fail");
                 $scope.errorLogin ="Error login";
+            }).finally(function(){
+                $scope.loaderLogin= false;
             });
-        };
+        },1000,1);
         
-        /**/
-    })
-    .config(function ($routeProvider,$locationProvider){
-        $routeProvider.when("/login",{
-            title: "LizTube - Login",
-            page: "Login",
-            controller: 'loginCtrl',
-            templateUrl: "login.html"
-        });
+    };
+    
+    /**/
+})
+.config(function ($routeProvider,$locationProvider){
+    $routeProvider.when("/login",{
+        title: "LizTube - Login",
+        page: "Login",
+        controller: 'loginCtrl',
+        templateUrl: "login.html"
     });
+});
 
