3c330ba1ed42d451dc7f3af767eda25a479f84eb
mpaloulack <mpaloulack@gmail.com>
2015-03-05 10:39:05 +0100

    UI register

--- FrontEndDev/resources/modules/user/register/register.js
+++ FrontEndDev/resources/modules/user/register/register.js
@@ -1,3 +1,33 @@
 /**
  * Created by Youcef on 26/02/2015.
  */
+angular.module("liztube.register",[
+    "ngRoute", 
+    "liztube.dataService.authService",
+    'ngMessages'
+]).controller("registerCtrl", function($scope, $location, authService, $window, $interval) {
+
+
+	$scope.user={
+        	firstname: '',
+		    lastname: '',
+		    pseudo: '',
+		    password: '',
+		    birthdate: '',
+		    email: '',
+    		isFemale: true
+        };
+    $scope.register= function() {
+        console.log(user);
+    };
+    
+    /**/
+})
+.config(function ($routeProvider,$locationProvider){
+    $routeProvider.when("/register",{
+        title: "LizTube - Enregistrement",
+        page: "Login",
+        controller: 'registerCtrl',
+        templateUrl: "register.html"
+    });
+});
