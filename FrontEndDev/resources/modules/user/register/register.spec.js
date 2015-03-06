b814a4d31cc43853885a7e85fe1fba9ca49c7c69
mpaloulack <mpaloulack@gmail.com>
2015-03-06 11:26:34 +0100

    Completion du register.spec
    manque les directives

--- FrontEndDev/resources/modules/user/register/register.spec.js
+++ FrontEndDev/resources/modules/user/register/register.spec.js
@@ -11,7 +11,7 @@ describe('liztube.register', function() {
 	beforeEach(module('liztube.register'));
 	beforeEach(module('liztube.dataService.authService'));
 
-	var $scope, $rootScope, $location, authService, $window, $q;
+	var $scope, $rootScope, $location, authService, $q/*, $setValidity*/;
 
 	beforeEach(inject(function (_$rootScope_, _$location_, _authService_, _$window_, _$q_) {
 		$rootScope =_$rootScope_;
@@ -19,6 +19,7 @@ describe('liztube.register', function() {
 		authService = _authService_;
 		$window= _$window_;
 	    $q = _$q_;
+	    //$setValidity = _$setValidity_;
 	}));
 
 	beforeEach(inject(function ($controller) {
@@ -34,31 +35,73 @@ describe('liztube.register', function() {
         createController();
     });
 
+    describe('variables', function(){
+
+        it('variables should initialized', function() {
+            expect($scope.errorRegister).toEqual('');
+        });
+
+    });
+
 
 
 	describe('Register', function() {
-		var userRegister;
+		var registerPromise ;
 
 		beforeEach(function(){
-	        userRegister = {
-				firstname: '',
-			    lastname: '',
-			    pseudo: '',
-			    birthdate: '',
-			    email: '',
-	            password: '',
-	            passwordConfirme: '',
-	    		isFemale: true
-			}
+	        registerPromise = $q.defer();
+            spyOn(authService, 'register').and.returnValue(registerPromise.promise);
     	});
 
-		describe('Variables', function() {
-			
+    	beforeEach(function(){
+            spyOn($rootScope, '$broadcast').and.callThrough();
+            spyOn($location,'path').and.callThrough();
+            $scope.register();
+        });
 
-			it('variables should initialized', function() {
-            	expect($scope.user).toEqual(userRegister);
-        	});
+    	it('should start global loading', function(){
+            expect($rootScope.$broadcast).toHaveBeenCalledWith('loadingStatus', true);
+        });
+
+        it('should stop global loading when request is finished', function(){
+            changePromiseResult(registerPromise, "failed");
+            expect($rootScope.$broadcast).toHaveBeenCalledWith('loadingStatus', false);
+        });
+
+
+    	it('should return an error message', function(){
+            changePromiseResult(registerPromise, "failed");
+            expect($scope.errorRegister).toEqual("Error Register");
+        });
+
+		it('should be a successful register', function() {
+			changePromiseResult(registerPromise, "resolve");
+			expect($location.path).toHaveBeenCalledWith('/login');
 		});
 	});
+
+	describe('Password verify', function() {
+		
+	});
+
+	describe('email validation', function() {
+		var emailVerifyPromise;
+
+		beforeEach(function(){
+	        registerPromise = $q.defer();
+            spyOn(authService, 'emailExist').and.returnValue(emailVerifyPromise.promise);
+    	});
+
+    	/*it('should return email validation error', function(){
+            changePromiseResult(emailVerifyPromise, "failed");
+            expect($setValidity).toEqual(false);
+        });*/
+
+	});
+
+	describe('pseudo validation', function() {
+		
+	});
+
 
 });
