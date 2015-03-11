fa6f04ced65837f56eb1d2d3a86bb6687c1a1130
Laurent <spywen@hotmail.fr>
2015-03-11 16:35:25 +0100

    Revert "fix tests toast"
    This reverts commit 62586a3cd58330339846bdcb153aa5c246dd3105.

--- FrontEndDev/resources/common/utils/toasts/toast.spec.js
+++ FrontEndDev/resources/common/utils/toasts/toast.spec.js
@@ -3,48 +3,20 @@ describe('liztube.moastr', function(){
     var moast, $mdToast;
 
     beforeEach(module('liztube.moastr'));
-
-    beforeEach(module(function($provide) {
-        $mdToast= {
-            show: function () {
-                return true;
-            }
-        };
-        $provide.constant('$mdToast', $mdToast);
-    }));
-
+/*
     beforeEach(inject(function (_moastr_) {
         moast = _moastr_;
-        spyOn($mdToast, 'show').and.callThrough();
+        $mdToast = {
+            error: function(){
+
+            }
+        };
     }));
 
     it('should toast an error', function(){
         moast.error("test");
-        expect($mdToast.show).toHaveBeenCalledWith({
-            template: '<md-toast class="moastr error"><span flex>test</span></md-toast>',
-            hideDelay: 6000,
-            position: 'left right bottom'
-        });
     });
-
-    it('should toast a warning', function(){
-        moast.success("test");
-        expect($mdToast.show).toHaveBeenCalledWith({
-            template: '<md-toast class="moastr success"><span flex>test</span></md-toast>',
-            hideDelay: 6000,
-            position: 'left right bottom'
-        });
-    });
-
-    it('should toast an info', function(){
-        moast.info("test");
-        expect($mdToast.show).toHaveBeenCalledWith({
-            template: '<md-toast class="moastr info"><span flex>test</span></md-toast>',
-            hideDelay: 6000,
-            position: 'left right bottom'
-        });
-    });
-
+*/
 
 
 });
